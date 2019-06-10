11c0352990fba86a718a4f1df541d291ecc90f61
yarun <yarun.luon@gmail.com>
2019-06-10 02:44:37 -0700

    Jukebox cycles through all songs

--- jukebox.py
+++ jukebox.py
@@ -1,33 +1,54 @@
-import time, os, random, subprocess
-import RPi.GPIO as GPIO
+"""
+Violin jukebox
 
-SONGS_DIR = '/home/pi/violin-jukebox/songs'
-GPIO_INPUT_PIN = 40
+Plays a random song when the violin is touched. Songs do not repeat until they've been all
+cycled through.
+yarun.luon@gmail.com
+"""
+
+import board, time, os, random, subprocess
+from digitalio import DigitalInOut, Direction
+
+# Constants
+SONGS_DIR = '/home/pi/repos/violin-jukebox/songs'
 OMXPLAYER_QUIT = 'q'.encode('utf-8')
+SONG_DURATION = 22
+PLAYLIST = tuple(os.listdir(SONGS_DIR))
+VOLUME_MAX = 200
+VOLUME_NORMAL = 0
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(GPIO_INPUT_PIN, GPIO.IN)
+# Uses Adafruit method. Alternative is directly use GPIO inputs
+pad_pin = board.D21
+pad = DigitalInOut(pad_pin)
+pad.direction = Direction.INPUT
+playlist = []
 
-playlist = os.listdir(SONGS_DIR)
+def print_playlist(playlist):
+  print("[ PLAYLIST ] [ PLAYLIST ] [ PLAYLIST ] [ PLAYLIST ]")
+  for index in range(len(playlist)):
+    print("{song_num}: {song_name}".format(song_num=index+1, song_name=playlist[index]))
+  print("♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪♫♪.ılılıll|̲̅̅●̲̅̅|̲̅̅=̲̅̅|̲̅̅●̲̅̅|llılılı.♫♪")
 
-# Print playlist
-for index in range(len(playlist)):
-  print("{song_num}: {song_name}".format(song_num=index+1, song_name=playlist[index]))
+def play(song_name):
+  filename = "{songs_dir}/{song}".format(songs_dir=SONGS_DIR, song=song_name)
+  command = "omxplayer --vol {volume} -o local '{filename}'".format(volume=VOLUME_NORMAL, filename=filename)
+  print("Command: {command}".format(command=command))
+  p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
+  time.sleep(SONG_DURATION)
+  p.communicate(input=OMXPLAYER_QUIT)
 
+print_playlist(PLAYLIST)
 
 while True:
-  # print(GPIO.input(GPIO_INPUT_PIN))
+  if pad.value:
+    print("pad touched. playing song")
 
-  value = GPIO.input(GPIO_INPUT_PIN)
-  if value == 1:
-    print("touched")
-    time.sleep(0.1)
-    song_id = random.choice(range(len(playlist)))
-     # Construct command
-    filename = "{songs_dir}/{song}".format(songs_dir=SONGS_DIR, song=playlist[song_id])
-    command = "omxplayer --vol 300 -o local '{filename}'".format(filename=filename)
-    print("Command: {command}".format(command=command))
+    if not playlist:
+      playlist = list(PLAYLIST[:])
+      random.shuffle(playlist)
 
-    p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
-    time.sleep(20)
-    p.communicate(input=OMXPLAYER_QUIT)
+    song_name = playlist.pop()
+    play(song_name)
+
+  # Not sure why I need to do this. Following sample code
+  time.sleep(0.1)
