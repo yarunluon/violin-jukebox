a6201ef497ab66857362d8491d8df5200bd7cf9e
yarun <yarun.luon@gmail.com>
2019-06-10 01:44:59 -0700

    User kill song code. WIP

--- jukebox.py
+++ jukebox.py
@@ -1,12 +1,19 @@
 import time, os, random, subprocess
-import RPi.GPIO as GPIO
 
-SONGS_DIR = '/home/pi/violin-jukebox/songs'
-GPIO_INPUT_PIN = 40
+import board
+from digitalio import DigitalInOut, Direction
+
+# Constants
+SONGS_DIR = '/home/pi/repos/violin-jukebox/songs'
+
 OMXPLAYER_QUIT = 'q'.encode('utf-8')
+SONG_DURATION = 22
+MODE_WAITING = "waiting"
+MODE_PLAYING = "playing"
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(GPIO_INPUT_PIN, GPIO.IN)
+pad_pin = board.D21
+pad = DigitalInOut(pad_pin)
+pad.direction = Direction.INPUT
 
 playlist = os.listdir(SONGS_DIR)
 
@@ -14,20 +21,30 @@ playlist = os.listdir(SONGS_DIR)
 for index in range(len(playlist)):
   print("{song_num}: {song_name}".format(song_num=index+1, song_name=playlist[index]))
 
+mode = MODE_WAITING
 
 while True:
-  # print(GPIO.input(GPIO_INPUT_PIN))
-
-  value = GPIO.input(GPIO_INPUT_PIN)
-  if value == 1:
-    print("touched")
+  if pad.value:
+    print(pad.value)
     time.sleep(0.1)
+    # if mode == MODE_WAITING:
     song_id = random.choice(range(len(playlist)))
-     # Construct command
+    # Construct command
     filename = "{songs_dir}/{song}".format(songs_dir=SONGS_DIR, song=playlist[song_id])
-    command = "omxplayer --vol 300 -o local '{filename}'".format(filename=filename)
+    command = "omxplayer --vol 200 -o local '{filename}'".format(filename=filename)
     print("Command: {command}".format(command=command))
 
+    mode = MODE_PLAYING
     p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
-    time.sleep(20)
+    time.sleep(SONG_DURATION)
+    print("end of song has arrived")
     p.communicate(input=OMXPLAYER_QUIT)
+    # time.sleep(5)
+      # mode = MODE_WAITING
+    # elif mode == MODE_PLAYING:
+    #   print("stopping song per request")
+    #   p.communicate(input=OMXPLAYER_QUIT)
+    #   time.sleep(1)
+    #   mode == MODE_WAITING
+  else:
+    time.sleep(0.1)
